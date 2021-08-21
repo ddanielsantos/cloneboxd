3b4376ba74ad5b70d53e53fb4391ab401e6ec032
ddanielsantos <contatodanieljob@gmail.com>
2021-08-21 10:01:30 -0300

    leftover line

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { getMovies } from '../controllers/requestMovies';
 const router = express.Router();
-const app = express()
 
 router.get('/', (req, res)=>{
     return res.send(`Home`)
