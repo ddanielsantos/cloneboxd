cec1761745bd4b2ceb773913dd10219dde300c9d
ddanielsantos <contatodanieljob@gmail.com>
2021-08-27 10:59:23 -0300

    added CORS support

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-require('dotenv/config')
+import cors from 'cors'
 import { router } from './src/routes/routes';
+require('dotenv/config')
 const port = process.env.PORT || 2020
 const app = express()
 
+app.use(cors())
 app.use(express.json()) // SEMPRE LEMBRAR DESSA PORCARIA ANTES DE QUALQUER UTILIZAÇÃO DO REQUEST
 
 app.use(router)
