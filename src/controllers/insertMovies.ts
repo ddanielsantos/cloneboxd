b21d7a2617689be260262bdcd439dac149604c33
ddanielsantos <contatodanieljob@gmail.com>
2021-09-07 00:08:59 -0300

    removed unused knex requirement

--- src/controllers/insertMovies.ts
+++ src/controllers/insertMovies.ts
@@ -1,11 +1,10 @@
-const knex = require("../../knex/knex")
 import { Request, Response } from "express";
 import { movie } from "../../types/types";
 import { genericInsert } from "./genericInsert";
 
 const insertMovies = async(req: Request, res: Response) =>{
     // console.log(req.body)
-    let data:movie = req.body
+    const data: movie = req.body
     
     for(const el in data){
         if(!data[el]) return res.status(500).send("Fill all the fields")
