f572ad0941cadda24df8e3c33d77268c0d2252c5
ddanielsantos <contatodanieljob@gmail.com>
2021-09-23 22:15:36 -0300

    standardizing imports

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-import { insertMovies } from '../controllers/movies/insertMovies'
-import { getPerson } from '../controllers/person/requestPerson'
-import { getMovies } from '../controllers/movies/requestMovies'
+import { createMovie } from '../controllers/movies/createMovie'
+import { getPerson } from '../controllers/person/requestPersons'
+import { getMovie } from '../controllers/movies/requestMovies'
 import { getRoles } from '../controllers/roles/requestRoles'
 import { auth } from '../controllers/user/authUser'
 import { createUser } from '../controllers/user/createUser'
@@ -9,14 +9,15 @@ import { createUser } from '../controllers/user/createUser'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  return res.send('Home')
+  return res.send('home')
 })
 
 router.post('/join', createUser)
 router.post('/login', auth)
-router.get('/movies', getMovies)
+router.get('/movies', getMovie)
 router.get('/roles', getRoles)
 router.get('/persons', getPerson)
 
-router.post('/movies', insertMovies)
+router.post('/movies', createMovie)
+
 export { router }
