0781c371291cc167a12ffdbb9cf0307f2b1559f9
ddanielsantos <contatodanieljob@gmail.com>
2021-09-29 22:09:29 -0300

    signing the jwt with user's uuid

--- src/controllers/user/authUser.ts
+++ src/controllers/user/authUser.ts
@@ -1,8 +1,13 @@
+import * as dotenv from 'dotenv'
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
-// import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { User } from '../../../types/types'
 import { knex } from '../../../knex/knex'
+
+dotenv.config()
+
+const secret = process.env.JWT_SECRET ?? 'wrong secret'
 
 const auth = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
   const credentials: User = req.body
@@ -23,9 +28,15 @@ const auth = async (req: Request, res: Response): Promise<Response<any, Record<s
 
     if (!passComparation) return res.status(500).send("the credentials didn't match")
 
-    return res.send('logged')
+    const oneMinuteToken: string = jwt.sign({ uuid: result[0].uuid }, secret, {
+      expiresIn: '15min'
+    })
+
+    return res.send({ token: oneMinuteToken })
   } catch {
     return res.status(500).send('an error occurred')
+
+    // usar o midd auth nas rotas e checar o funcionamento
   }
 }
 
