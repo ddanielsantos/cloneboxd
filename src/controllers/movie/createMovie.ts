597d94664131384cc8aa37b2731da76d52f77673
ddanielsantos <contatodanieljob@gmail.com>
2021-10-18 23:41:59 -0300

    improved  database error handling

--- src/controllers/movie/createMovie.ts
+++ src/controllers/movie/createMovie.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express'
 import { Movie } from '../../../types/types'
+import { databaseErrorList } from 'helpers/errors/errors'
 import MovieRepository from 'repositories/MovieRepository'
 
 const createMovie = async (req: Request, res: Response): Promise<Response> => {
   const data: Movie = req.body
+
+  if (data.id != null) {
+    return res.status(400).send('you cannot specify an id')
+  }
 
   for (const property in data) {
     if (data[property] === '') {
@@ -19,9 +24,11 @@ const createMovie = async (req: Request, res: Response): Promise<Response> => {
     await MovieRepository.insert(data)
 
     return res.status(200).send('movie added')
-  } catch (err) {
-    if (err.code === '23502') {
-      return res.status(400).send('some data wasnt sent')
+  } catch (insertionError) {
+    for (const element of databaseErrorList) {
+      if (element.code === insertionError.code) {
+        return res.status(400).send(element.message)
+      }
     }
     return res.status(400).send('an error occurred')
   }
---
+++ src/helpers/errors/errors.ts
@@ -0,0 +1,6 @@
+export const databaseErrorList = [
+  { code: '42703', message: 'an unknown property has been sent' },
+  { code: '23502', message: 'a mandatory property was not sent' },
+  { code: '22P02', message: 'a property was submitted with the wrong type.' },
+  { code: '23505', message: 'one of the submitted properties is unique and has already been provided' }
+]
