2c76c4531eddd1be4523cbf244a754059782eba7
ddanielsantos <contatodanieljob@gmail.com>
2021-10-22 19:41:59 -0300

    checking data properties before anything

--- src/controllers/movie/createMovie.ts
+++ src/controllers/movie/createMovie.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express'
 import { Movie } from '../../../types/types'
 import { databaseErrorList } from 'helpers/errors/errors'
+import { isMovie } from 'helpers/validators/runtimeTypeGuards'
 import MovieRepository from 'repositories/MovieRepository'
 
 const createMovie = async (req: Request, res: Response): Promise<Response> => {
   const data: Movie = req.body
+
+  if (!isMovie(data)) {
+    return res.status(400).send('cacetes')
+  }
+
+  if (req.headers['content-type'] !== 'application/json') {
+    return res.status(400).send('the content type must be "application/json"')
+  }
 
   if (data.id != null) {
     return res.status(400).send('you cannot specify an id')
---
+++ src/helpers/validators/runtimeTypeGuards.ts
@@ -0,0 +1,23 @@
+import { Movie } from '../../../types/types'
+
+type inputted<T> = Omit<T, 'id'>
+
+type movieInput = inputted<Movie>
+
+const hasProperty = (prop: any): boolean => {
+  if (prop === undefined) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export const isMovie = (data: movieInput): boolean => {
+  const hasPropertyResult = [data.title, data.duration, data.origin, data.year].map(hasProperty)
+
+  if (hasPropertyResult.includes(false)) {
+    return false
+  } else {
+    return true
+  }
+}
