72d3333a9694df6e9d7b49d9aa4c2be4e7b51ad0
ddanielsantos <contatodanieljob@gmail.com>
2021-10-22 19:40:59 -0300

    adding interface for errors

--- src/helpers/errors/errors.ts
+++ src/helpers/errors/errors.ts
@@ -1,4 +1,9 @@
-export const databaseErrorList = [
+interface codeAndMessage {
+  code: string | number
+  message: string
+}
+
+export const databaseErrorList: codeAndMessage[] = [
   { code: '42703', message: 'an unknown property has been sent' },
   { code: '23502', message: 'a mandatory property was not sent' },
   { code: '22P02', message: 'a property was submitted with the wrong type.' },
