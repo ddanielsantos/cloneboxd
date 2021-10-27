3670684b03ba4b33f3366ad647f109d7057c8dd6
ddanielsantos <contatodanieljob@gmail.com>
2021-10-26 21:30:19 -0300

    decoupling movie validation to another file/layer

--- src/controllers/movie/createMovie.ts
+++ src/controllers/movie/createMovie.ts
@@ -1,45 +1,34 @@
 import { Request, Response } from 'express'
 import { Movie } from '../../../types/types'
 import { databaseErrorList } from 'helpers/errors/errors'
-import { isMovie } from 'helpers/validators/runtimeTypeGuards'
 import MovieRepository from 'repositories/MovieRepository'
+import { validateMovie } from 'helpers/validators/validateMovie'
 
 const createMovie = async (req: Request, res: Response): Promise<Response> => {
-  const data: Movie = req.body
-
-  if (!isMovie(data)) {
-    return res.status(400).send('cacetes')
-  }
-
   if (req.headers['content-type'] !== 'application/json') {
     return res.status(400).send('the content type must be "application/json"')
   }
 
-  if (data.id != null) {
-    return res.status(400).send('you cannot specify an id')
-  }
+  const data: Movie = req.body
 
-  for (const property in data) {
-    if (data[property] === '') {
-      return res.status(400).send('fill all the fields')
-    }
+  const validMovie = validateMovie(data)
 
-    if (data.year === 0) {
-      return res.status(400).send('the year of release cannot be 0')
-    }
+  if ('error' in validMovie) {
+    return res.status(400).send(validMovie.error)
   }
 
   try {
     await MovieRepository.insert(data)
 
-    return res.status(200).send('movie added')
+    return res.status(201).send('movie added')
   } catch (insertionError) {
     for (const element of databaseErrorList) {
       if (element.code === insertionError.code) {
         return res.status(400).send(element.message)
       }
     }
-    return res.status(400).send('an error occurred')
+
+    return res.status(500).send('an error occurred')
   }
 }
 
