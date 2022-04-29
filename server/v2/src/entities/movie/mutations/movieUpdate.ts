ab7f6fc337e044c409c265a9644f66b85c26929e
Daniel Santos <contatodanieljob@gmail.com>
2022-04-29 12:07:27 -0300

    feat: prevent invalid crew members

--- server/v2/src/entities/movie/mutations/movieUpdate.ts
+++ server/v2/src/entities/movie/mutations/movieUpdate.ts
@@ -4,8 +4,9 @@ import {
   GraphQLNonNull
 } from 'graphql'
 import { movieInputType } from '../movieTypes'
-import { movieRepository } from '../movieRepository'
+import { Movie, movieRepository } from '../movieRepository'
 import { fromGlobalId, mutationWithClientMutationId } from 'graphql-relay'
+import { crewRepository } from '../../../entities/crew/crewRepository'
 
 export const movieUpdate = mutationWithClientMutationId({
   name: 'movieUpdate',
@@ -22,7 +23,18 @@ export const movieUpdate = mutationWithClientMutationId({
       resolve: response => response.modifiedCount
     }
   },
-  mutateAndGetPayload: async ({ id, ...movie }) => {
+  mutateAndGetPayload: async ({ id, ...movie }: Movie & { id: string }) => {
+    const validActors = await crewRepository.findMany(movie.actors)
+    const validDirectors = await crewRepository.findMany(movie.directors)
+
+    if (validActors.length !== movie.actors.length) {
+      throw new Error('All actors must be valid members of the crew')
+    }
+
+    if (validDirectors.length !== movie.directors.length) {
+      throw new Error('All directors must be valid members of the crew')
+    }
+
     return (await movieRepository.updateOne(fromGlobalId(id).id, movie))
   }
 })
