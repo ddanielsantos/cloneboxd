2ed9a9d1b214300c60442a35258b920c78331f91
Daniel Santos <contatodanieljob@gmail.com>
2022-05-05 20:22:44 -0300

    fix: fix vite to work with React 18 and Relay

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,15 @@
 import App from './App'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
