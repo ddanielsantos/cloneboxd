02bc97ba4d279e637e039604f752b0ea5a3bda2a
Daniel Santos <contatodanieljob@gmail.com>
2022-05-05 20:23:37 -0300

    feat: add fetch gql to login page

---
+++ web/fecthGraphQL.ts
@@ -0,0 +1,18 @@
+async function fetchGraphQL(text: string, variables: {}) {
+  // Fetch data from GitHub's GraphQL API:
+  const response = await fetch('http://localhost:4000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+}
+
+export default fetchGraphQL;
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import * as yup from 'yup'
 import { useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react'
+import fetchGraphQL from '../../fecthGraphQL'
 import { yupResolver } from '@hookform/resolvers/yup'
 import {
   Stack,
@@ -22,11 +24,44 @@ const schema = yup.object({
 }).required('Os dados de login são obrigatórios')
 
 export const Login = () => {
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    let isMounted = true
+
+    const handleLogin = async () => {
+      const response = await fetchGraphQL(`
+      mutation a {
+        loginUser (input: {
+          email: "tester@mail.com",
+          password: "123456"
+        }) {
+          token
+          error
+        }
+      }
+      `, {})
+
+      if (!isMounted) {
+        return
+      }
+
+      setToken(response.data.loginUser.token)
+    }
+
+    handleLogin()
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: yupResolver(schema)
   })
 
   const onSubmit = (data: FormData) => {
+    console.log(token)
     console.log('chamar o relay e passar: ', data)
   }
 
