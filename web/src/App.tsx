f8131e1b65653376f7464f8ba148ddc869d75ac0
Daniel Santos <contatodanieljob@gmail.com>
2022-05-13 19:44:42 -0300

    refactor(web): componetize ThemeSwitcher button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,16 @@
-import {
-  HiOutlineSun,
-  HiOutlineMoon
-} from 'react-icons/hi'
-import {
-  Center,
-  useColorMode,
-  Circle
-} from '@chakra-ui/react'
-import RelayEnvironment from './relay/environment'
-import { RelayEnvironmentProvider } from 'react-relay/hooks'
 import { Routes } from './routes/authRoute'
+import RelayEnvironment from './relay/environment'
+import { Center, useColorMode } from '@chakra-ui/react'
+import { RelayEnvironmentProvider } from 'react-relay/hooks'
 
 function App() {
-  const { colorMode, setColorMode } = useColorMode()
+  const { colorMode } = useColorMode()
 
   return (
     <>
       <Center h={'100vh'} backgroundColor={colorMode === 'light' ? 'white' : 'gray.800'}>
         <Routes />
       </Center>
-
-      <Circle
-        position={'absolute'}
-        top={0}
-        right={0}
-        onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-        size={'3em'}
-      >
-        {colorMode === 'dark' ? <HiOutlineSun size={'1.5em'} /> : <HiOutlineMoon size={'1.5em'} />}
-      </Circle>
     </>
   )
 }
---
+++ web/src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,20 @@
+import { Button, useColorMode } from "@chakra-ui/react"
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi"
+
+export const ThemeSwitcher = () => {
+  const { colorMode, setColorMode } = useColorMode()
+
+  return (
+    <Button
+      size={'3em'}
+      variant={'ghost'}
+      onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+    >
+      {
+        colorMode === 'dark'
+          ? <HiOutlineMoon size={'1.5em'} />
+          : <HiOutlineSun size={'1.5em'} />
+      }
+    </Button>
+  )
+}
