6fd1ab9d0db2724e34330e77b2f1c6a1af26a974
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-06-06 21:59:52 -0300

    Revert "feat: decouple auth required logic into a component"

--- web/src/routes/authRoute.tsx
+++ web/src/routes/authRoute.tsx
@@ -1,16 +1,31 @@
 import { Home } from '../pages/Home/Home'
 import { Login } from '../pages/Login/Login'
+import { AuthRequired } from './AuthRequired'
 import { SignUp } from '../pages/SignUp/SignUp'
-import { Review } from '../pages/Review/Review'
 import { NoMatch } from '../pages/NoMatch/NoMatch'
 import { Routes as RRoutes, Route } from 'react-router-dom'
-import { ProtectedRoute } from '../helpers/ProtectedRoute'
+import { Review } from '../pages/Review/Review'
 
 export const Routes = () => {
   return (
     <RRoutes>
-      <ProtectedRoute path='/' element={<Home />} />
-      <ProtectedRoute path='/review' element={<Review />} />
+      {/* TODO: #22 transform this in a component */}
+      <Route
+        path="/"
+        element={
+          <AuthRequired>
+            <Home />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path="/review"
+        element={
+          <AuthRequired>
+            <Review />
+          </AuthRequired>
+        }
+      />
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NoMatch />} />
