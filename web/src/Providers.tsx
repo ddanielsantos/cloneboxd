8ba8a6ef4edc04362e45c9ac911dc52e7822db1b
Daniel Santos <contatodanieljob@gmail.com>
2022-06-25 15:56:07 -0300

    style: change font

--- web/src/Providers.tsx
+++ web/src/Providers.tsx
@@ -1,3 +1,9 @@
+import theme from './theme'
+import '@fontsource/inter/800.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/400.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import RelayEnvironment from './relay/environment'
@@ -8,7 +14,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
       <BrowserRouter>
-        <ChakraProvider >
+        <ChakraProvider
+          theme={theme}
+        >
           <AuthProvider>
             {children}
           </AuthProvider>
