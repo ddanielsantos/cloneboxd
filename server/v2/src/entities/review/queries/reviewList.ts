2e7fef8eb5699791442b3e0d19eb02bf8dcddc48
Daniel Santos <contatodanieljob@gmail.com>
2022-07-09 11:52:16 -0300

    feat: add sort and filter

--- server/v2/src/entities/review/queries/reviewList.ts
+++ server/v2/src/entities/review/queries/reviewList.ts
@@ -1,13 +1,39 @@
-import { GraphQLFieldConfig } from 'graphql'
+import { GraphQLFieldConfig, GraphQLFieldConfigArgumentMap, GraphQLInt, GraphQLString } from 'graphql'
 import { ReviewConnection } from '../reviewTypes'
 import { ReviewModel } from '../reviewModel'
 import { connectionArgs, connectionFromArray } from 'graphql-relay'
 
+type Args = GraphQLFieldConfigArgumentMap & {
+  sort?: string
+  rating?: number
+  direction?: -1 | 1
+}
+
 export const reviewList: GraphQLFieldConfig<any, any, any> = {
   type: ReviewConnection,
-  args: connectionArgs,
-  resolve: async (_, args) => {
-    const reviews = await ReviewModel.find({})
-    return connectionFromArray(reviews, args)
+  args: {
+    ...connectionArgs,
+    sort: {
+      type: GraphQLString,
+      description: 'Sort reviews by this field'
+    },
+    direction: {
+      type: GraphQLString,
+      description: 'Use the sort field in this direction'
+    },
+    rating: {
+      type: GraphQLInt,
+      description: 'Filter reviews by this rating'
+    }
+  },
+  resolve: async (_, args: Args) => {
+    const { sort, rating, direction, ...connnectionArgs } = args
+
+    const reviews = await ReviewModel.aggregate([
+      { $match: rating ? { rating } : {} },
+      { $sort: { [sort || 'watchedAt']: direction || -1 } }
+    ])
+
+    return connectionFromArray(reviews, connnectionArgs)
   }
 }
