a362a6fa98408be6f354bacae4f11f0c410efe2e
Daniel Santos <contatodanieljob@gmail.com>
2022-07-13 11:59:05 -0300

    test(review): add create review fixture

---
+++ server/v2/src/entities/review/fixtures/createReview.ts
@@ -0,0 +1,22 @@
+import { ReviewModel } from '../reviewModel'
+import { createUser } from '../../user/fixture/createUser'
+
+export async function createReview() {
+  const review = await ReviewModel.findOne({ text: 'cool fixture', movie: '11220' })
+
+  if (review) return review
+
+  const user = await createUser({ admin: false })
+
+  const document = new ReviewModel({
+    text: 'cool fixture',
+    rating: 5,
+    watchedAt: new Date(),
+    user: user.id,
+    movie: '11220'
+  })
+
+  await document.save()
+
+  return document
+}
