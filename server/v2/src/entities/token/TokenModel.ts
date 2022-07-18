ee88789b3d107cd4a303d3541b28857c30785116
Daniel Santos <contatodanieljob@gmail.com>
2022-07-18 16:25:10 -0300

    WIP(token): add statics to manage token creation

--- server/v2/src/entities/token/TokenModel.ts
+++ server/v2/src/entities/token/TokenModel.ts
@@ -1,4 +1,6 @@
 import mongoose, { Model, Schema } from 'mongoose'
+import { sign, verify, JwtPayload } from 'jsonwebtoken'
+import { getEnvironmentVariables } from '../../config/env'
 
 export interface IToken {
   user: Schema.Types.ObjectId
@@ -6,12 +8,12 @@ export interface IToken {
   refreshToken: string
 }
 
-// type AccessToken = string
+type AccessToken = string
 
 interface TokenModel extends Model<IToken> {
-  // generateRefreshToken: (user: string) => Promise<IToken>
+  generateRefreshToken: (user: string) => Promise<IToken>
   generateAccessToken: (user: string) => string
-  // refresh: (refreshToken: string) => Promise<AccessToken>
+  refresh: (refreshToken: string) => Promise<AccessToken>
   // revoke: (user: string) => Promise<void>
 }
 
@@ -29,6 +31,74 @@ const schema = new Schema<IToken, TokenModel>({
     type: Date,
     required: true
   }
+})
+
+type UserIdIfNotStale = {
+  stale: true
+} | {
+  stale: false
+  id: string
+}
+
+function userIdIfNotStale(token: string, secret: string): UserIdIfNotStale {
+  const actualDate = Math.floor(Date.now() / 1000) + 14000
+  const decoded = verify(token, secret) as JwtPayload
+  const expireDate = decoded.exp!
+
+  if (actualDate > expireDate) {
+    return { stale: true }
+  }
+
+  return { stale: false, id: decoded.id }
+}
+
+schema.static('generateRefreshToken', async function (user: string) {
+  const REFRESH_TOKEN_SECRET = getEnvironmentVariables().REFRESH_TOKEN_SECRET || ''
+  const SEVEN_DAYS_IN_SECONDS = 7 * 24 * 60 * 60
+
+  const token = sign({
+    id: user
+  }, REFRESH_TOKEN_SECRET, {
+    expiresIn: SEVEN_DAYS_IN_SECONDS
+  })
+
+  const expiresIn = new Date(SEVEN_DAYS_IN_SECONDS * 1000)
+
+  return await this.findOneAndReplace({ user }, {
+    user,
+    expiresIn,
+    refreshToken: token
+  }, { new: true, upsert: true })
+    .select('-user -_id')
+})
+
+schema.static('generateAccessToken', function (user: string) {
+  const ACCESS_TOKEN_SECRET = getEnvironmentVariables().ACCESS_TOKEN_SECRET || ''
+
+  const token = sign({
+    id: user
+  }, ACCESS_TOKEN_SECRET, {
+    expiresIn: '15min'
+  })
+
+  return token
+})
+
+schema.static('refresh', async function (refreshToken: string): Promise<AccessToken> {
+  const REFRESH_TOKEN_SECRET = getEnvironmentVariables().REFRESH_TOKEN_SECRET || ''
+  const JWT = userIdIfNotStale(refreshToken, REFRESH_TOKEN_SECRET)
+
+  if (JWT.stale) {
+    throw new Error('Token is stale')
+  }
+
+  const token = await this.findOne({ user: JWT.id })
+
+  if (!token) {
+    throw new Error('User doesnt have a refresh token')
+  }
+
+  return this.generateAccessToken(token.user.toString())
 })
 
 export const Token = mongoose.model<IToken, TokenModel>('token', schema)
