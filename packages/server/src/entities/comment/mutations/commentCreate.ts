45454341d3afc0cc53a065e404f9810be6ea1f5c
Daniel Santos <contatodanieljob@gmail.com>
2022-07-29 12:13:36 -0300

    feat(coment): add comment create mutation

---
+++ packages/server/src/entities/comment/mutations/commentCreate.ts
@@ -0,0 +1,70 @@
+import {
+  fromGlobalId,
+  mutationWithClientMutationId
+} from 'graphql-relay'
+import { Types } from 'mongoose'
+import { commentType } from '../commentTypes'
+import { getHeadersPayload } from '../../../auth/getHeadersPayload'
+import { errorField } from '../../../graphql/errorField'
+import { GraphQLNonNull, GraphQLID, GraphQLString } from 'graphql'
+import { ReviewModel } from '../../review/reviewModel'
+
+type Comment = {
+  review: string
+  content: string
+}
+
+export const commentCreate = mutationWithClientMutationId({
+  name: 'commentCreate',
+  description: 'Add a comment',
+  inputFields: {
+    review: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'The id of the review to comment on'
+    },
+    content: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The content of the comment'
+    }
+  },
+  outputFields: {
+    comment: {
+      type: commentType,
+      resolve: response => response.comment
+    },
+    ...errorField
+  },
+  mutateAndGetPayload: async ({ ...comment }: Comment, ctx) => {
+    const { error, payload } = getHeadersPayload(ctx)
+
+    if (error || !payload) {
+      return {
+        error: 'Unauthorized',
+        comment: null
+      }
+    }
+
+    const { id: _id } = fromGlobalId(comment.review)
+
+    const user = new Types.ObjectId(payload.id)
+
+    try {
+      const result = await ReviewModel.findOneAndUpdate({ _id }, {
+        $push: {
+          comments: {
+            content: comment.content,
+            user
+          }
+        }
+      }, { new: true })
+
+      return {
+        comment: result?.comments.at(-1)
+      }
+    } catch (error: unknown) {
+      return {
+        error: (error as Error).message
+      }
+    }
+  }
+})
--- packages/server/src/schemas/mutation.ts
+++ packages/server/src/schemas/mutation.ts
@@ -4,12 +4,14 @@ import {
 
 import { userMutations } from '../entities/user/mutations/user'
 import { reviewMutations } from '../entities/review/mutations/review'
+import { commentMutations } from '../entities/comment/mutations/comment'
 
 export const mutation = new GraphQLObjectType({
   name: 'Mutation',
   description: 'The root mutation type',
   fields: {
     ...userMutations,
+    ...commentMutations,
     ...reviewMutations
   }
 })
