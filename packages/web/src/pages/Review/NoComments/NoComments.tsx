b03b5b87d435ba393bcfced6057c534044e4df24
Daniel Santos <contatodanieljob@gmail.com>
2022-08-02 09:05:14 -0300

    feat(review): add alert if no comments found

---
+++ packages/web/src/pages/Review/NoComments/NoComments.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export const NoComments = () => {
+  return (
+    <Box>
+      <Text
+        textAlign={'center'}
+      >
+        There&apos;re no comments on this review
+      </Text>
+    </Box>
+  )
+}
