12a14f5ef4fc70d5c4e24cc19cc1e4749d076c53
ddanielsantos <contatodanieljob@gmail.com>
2022-08-26 19:40:52 -0300

    refactor: improve description

--- packages/server/src/entities/credit/creditType.ts
+++ packages/server/src/entities/credit/creditType.ts
@@ -1,18 +1,18 @@
-import { personType } from '../crew/personTypes'
+import { personType } from '../person/personTypes'
 import { GraphQLObjectType, GraphQLNonNull, GraphQLString } from 'graphql'
 
 export const creditType = new GraphQLObjectType({
   name: 'Credit',
-  description: 'Credit type',
+  description: 'Who made what on this movie development',
   fields: () => ({
     person: {
       type: new GraphQLNonNull(personType),
-      description: `Credit's actor`,
+      description: `Who played this role`,
       resolve: cast => cast.person
     },
     role: {
       type: new GraphQLNonNull(GraphQLString),
-      description: `Person's role`,
+      description: `What was the role of this person on this movie development`,
       resolve: cast => cast.role
     }
   })
